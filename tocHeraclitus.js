bf871d1f4de59f8a5f750bafacf90a72981e7538
siebentod <33163165+siebentod@users.noreply.github.com>
2023-07-08 18:28:53 +0300

    cosmetique

--- tocHeraclitus.js
+++ tocHeraclitus.js
@@ -1,55 +1,55 @@
 function translChange() {
-    let showM = document.getElementById("makovelsky");
+    let showM = document.getElementById("chooseMakovelsky");
     showM.addEventListener("click", function() {
+        //change translation
         clearEndsOfColumns();
-
         document.querySelector(".endOfMakov").classList.add("hide");
-
+        //boldness
         unBoldAll();
-        document.getElementById("makovelsky").style.fontWeight = "bold";
-
+        document.getElementById("chooseMakovelsky").dataset.selected = true;
+        //change text in header
         document.querySelector(".translator").innerHTML = "Перевод А.О. Маковельского";
     });
     
-    let showD = document.getElementById("dynnik");
+    let showD = document.getElementById("chooseDynnik");
     showD.addEventListener("click", function() {
+        //change translation
         clearEndsOfColumns();
-
         document.querySelector(".endOfColumn2").classList.add("hide");
         document.querySelector(".endOfMakov").classList.add("show");
         document.querySelector(".endOfDyn").classList.add("hideFurther");
-
+        //boldness
         unBoldAll();
-        document.getElementById("dynnik").style.fontWeight = "bold";
-
+        document.getElementById("chooseDynnik").dataset.selected = true;
+        //change text in header
         document.querySelector(".translator").innerHTML = "Перевод М.А. Дынника";
     });
 
-    let showL = document.getElementById("lebedev2014");
+    let showL = document.getElementById("chooseLebedev2014");
     showL.addEventListener("click", function() {
+        //change translation
         clearEndsOfColumns();
-
         document.querySelector(".endOfColumn2").classList.add("hide");
         document.querySelector(".endOfDyn").classList.add("show");
         document.querySelector(".endOfLeb").classList.add("hideFurther");
-
+        //boldness
         unBoldAll();
-        document.getElementById("lebedev2014").style.fontWeight = "bold";
-
+        document.getElementById("chooseLebedev2014").dataset.selected = true;
+        //change text in header
         document.querySelector(".translator").innerHTML = 'Перевод <a href="https://www.academia.edu/13585247">А.В. Лебедева 2014</a>';
     });
 
-    let showL2 = document.getElementById("lebedev1989");
+    let showL2 = document.getElementById("chooseLebedev1989");
     showL2.addEventListener("click", function() {
+        //change translation
         clearEndsOfColumns();
-
         document.querySelector(".endOfColumn2").classList.add("hide");
         document.querySelector(".endOfDyn").classList.add("hide");
         document.querySelector(".endOfLeb").classList.add("show");
-
+        //boldness
         unBoldAll();
-        document.getElementById("lebedev1989").style.fontWeight = "bold";
-
+        document.getElementById("chooseLebedev1989").dataset.selected = true;
+        //change text in header
         document.querySelector(".translator").innerHTML = "Перевод А.В. Лебедева 1989";
     });
 
@@ -73,10 +73,10 @@ function clearEndsOfColumns(){
 }
 
 function unBoldAll(){
-    document.getElementById("makovelsky").style.fontWeight = "normal";
-    document.getElementById("dynnik").style.fontWeight = "normal";
-    document.getElementById("lebedev2014").style.fontWeight = "normal";
-    document.getElementById("lebedev1989").style.fontWeight = "normal";
+    const translators = document.querySelectorAll('.chooseTranslator')
+    translators.forEach(translator => {
+        translator.dataset.selected = false;
+    });
 }
 
 translChange();
